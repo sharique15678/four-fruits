75e580657579b57fadac3836f970a399e7f753e0
Md Sharique <sharique15678@gmail.com>
2021-02-08 17:54:02 +0530

    finally

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask , jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from four fruits'
+    
+@app.route('/send')
+def send():
+    dict = {
+        "name" : "Md sharique",
+        "age" : 12
+        }
+    return jsonify(dict)
